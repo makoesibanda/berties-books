8657485e2c6db9aeddecbf7c72a177940bf221a8
makoesibanda <makoesibanda@gmail.com>
2025-11-12 03:05:55 +0000

    Intial Forked Version

---
+++ routes/books.js
@@ -0,0 +1,28 @@
+// Create a new router
+const express = require("express")
+const router = express.Router()
+
+router.get('/search',function(req, res, next){
+    res.render("search.ejs")
+});
+
+router.get('/search-result', function (req, res, next) {
+    //searching in the database
+    res.send("You searched for: " + req.query.keyword)
+});
+
+//display all books /
+    router.get('/list', function(req, res, next) {
+        let sqlquery = "SELECT * FROM books"; // query database to get all the books
+        // execute sql query
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                next(err)
+            }
+            res.send(result)
+         });
+    });
+
+
+// Export the router object so index.js can access it
+module.exports = router
